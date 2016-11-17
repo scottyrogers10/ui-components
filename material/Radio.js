e1133da1aff0ad5dceb598fe7f6f322966fc2aa1
Scotty Rogers <scotty.rogers10@gmail.com>
2016-11-17 13:09:46 -0700

    Added in 2 new components.. a loader circle and radio buttons

---
+++ material/Radio.js
@@ -0,0 +1,95 @@
+﻿BASE.require([
+    "jQuery",
+    "BASE.web.animation.PercentageTimeline",
+    "BASE.web.animation.ElementAnimation"
+], function () {
+
+    BASE.namespace("app.components.material");
+
+    var PercentageTimeline = BASE.web.animation.PercentageTimeline;
+    var ElementAnimation = BASE.web.animation.ElementAnimation;
+
+    var createScaleAnimation = function (element) {
+        var scaleAnimation = new ElementAnimation({
+            target: element,
+            properties: {
+                scaleX: {
+                    from: "0",
+                    to: "1"
+                },
+                scaleY: {
+                    from: "0",
+                    to: "1"
+                }
+            },
+            easing: "easeOutQuad"
+        });
+
+        var timeline = new PercentageTimeline(100);
+
+        timeline.add({
+            animation: scaleAnimation,
+            startAt: 0,
+            endAt: 1
+        });
+
+        return timeline;
+    };
+
+    app.components.material.Radio = function (elem, tags, services) {
+        var self = this;
+        var $elem = $(elem);
+        var $label = $(tags["label"]);
+        var $checkedIcon = $(tags["checked-icon"]);
+        var $uncheckedIcon = $(tags["unchecked-icon"]);
+
+        var changeState = null;
+        var scaleAnimation = createScaleAnimation($checkedIcon[0]);
+
+        var radioStateMachine = {
+            checked: function () {
+                scaleAnimation.seek(0).playToEndAsync().try();
+                changeState = "unchecked";
+            },
+            unchecked: function () {
+                scaleAnimation.seek(1).reverseToStartAsync().try();
+                changeState = "checked";
+            }
+        };
+
+        var init = function () {
+            var config = {
+                label: $elem.attr("label"),
+                labelColor: $elem.attr("label-color"),
+                color: $elem.attr("color")
+            };
+
+            self.setConfig(config);
+            changeState = "unchecked";
+        };
+
+        self.setConfig = function (config) {
+            var label = config.label || "";
+            var labelColor = config.labelColor || "#5b5b5b"
+            var color = config.color || "#424242";
+
+            $label.text(label);
+            $label.css("color", labelColor);
+            $checkedIcon.css("background-color", color);
+            $uncheckedIcon.css("color", color);
+
+            $checkedIcon.removeClass("hide");
+            $uncheckedIcon.removeClass("hide");
+        };
+
+        self.isChecked = function () {
+            return changeState === "unchecked" ? true : false;
+        };
+
+        $elem.on("click", function () {
+            radioStateMachine[changeState]();
+        });
+
+        init();
+    };
+});
---
+++ material/Spinner.js
@@ -0,0 +1,74 @@
+﻿BASE.require([
+    "jQuery",
+    "BASE.web.animation.PercentageTimeline",
+    "BASE.web.animation.ElementAnimation"
+], function () {
+
+    BASE.namespace("app.components.material");
+
+    var PercentageTimeline = BASE.web.animation.PercentageTimeline;
+    var ElementAnimation = BASE.web.animation.ElementAnimation;
+
+    var createRotatingAnimation = function (spinnerElement) {
+        var rotatingAnimation = new ElementAnimation({
+            target: spinnerElement,
+            easing: "linear",
+            properties: {
+                rotateZ: {
+                    from: "0deg",
+                    to: "360deg"
+                }
+            }
+        });
+
+        var timeline = new PercentageTimeline(1000);
+
+        timeline.add({
+            animation: rotatingAnimation,
+            startAt: 0,
+            endAt: 1
+        });
+
+        return timeline;
+    };
+
+    app.components.material.Spinner = function (elem, tags, services) {
+        var self = this;
+        var $elem = $(elem);
+        var rotatingAnimation = createRotatingAnimation($elem[0]);
+
+        var init = function () {
+            var config = {
+                size: $elem.attr("size"),
+                color: $elem.attr("color")
+            };
+
+            self.setConfig(config);
+            rotatingAnimation.repeat = Infinity;
+            self.play();
+        };
+
+        self.setConfig = function (config) {
+            var size = config.size || 40;
+            var color = config.color || "#2196f3";
+            var thickness = Math.floor(size / 12);
+
+            $elem.css({
+                "width": size + "px",
+                "height": size + "px",
+                "border": thickness + "px " + "solid " + "transparent",
+                "border-top": thickness + "px " + "solid " + color
+            });
+        };
+
+        self.play = function () {
+            rotatingAnimation.playToEndAsync().try();
+        };
+
+        self.pause = function () {
+            rotatingAnimation.pause();
+        };
+
+        init();
+    };
+});
